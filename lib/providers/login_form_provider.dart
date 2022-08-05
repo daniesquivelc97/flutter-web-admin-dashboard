7aa63aadfcfcfc3a83f912131a3d44fd6c045a09
daniesquivelc97 <dani_esquivelc@hotmail.com>
2022-08-05 16:14:35 -0500

    Form provider implementation and password validation

---
+++ lib/providers/login_form_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginFormProvider extends ChangeNotifier {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  String email = '';
+  String password = '';
+
+  validateForm() {
+    if (formKey.currentState!.validate()) {
+      print('Form valid... Login');
+    } else {
+      print('Form not valid');
+    }
+  }
+}
--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -1,60 +1,83 @@
+import 'package:admin_dashboard/providers/login_form_provider.dart';
 import 'package:admin_dashboard/router/router.dart';
 import 'package:admin_dashboard/ui/buttons/custom_outlined_button.dart';
 import 'package:admin_dashboard/ui/buttons/link_text.dart';
 import 'package:admin_dashboard/ui/inputs/custom_inputs.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(top: 100),
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Center(
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 370),
-          child: Form(
-              child: Column(
-            children: [
-              // Email
-              TextFormField(
-                // validator: (value) => ,
-                style: const TextStyle(color: Colors.white),
-                decoration: CustomInputs.loginInputDecoration(
-                  hint: 'Ingrese su e-mail',
-                  label: 'Email',
-                  icon: Icons.email_outlined,
-                ),
+    return ChangeNotifierProvider(
+      create: (_) => LoginFormProvider(),
+      child: Builder(
+        builder: (context) {
+          final loginFormProvider =
+              Provider.of<LoginFormProvider>(context, listen: false);
+          return Container(
+            margin: const EdgeInsets.only(top: 100),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Center(
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 370),
+                child: Form(
+                    key: loginFormProvider.formKey,
+                    child: Column(
+                      children: [
+                        // Email
+                        TextFormField(
+                          // validator: (value) => ,
+                          style: const TextStyle(color: Colors.white),
+                          decoration: CustomInputs.loginInputDecoration(
+                            hint: 'Ingrese su e-mail',
+                            label: 'Email',
+                            icon: Icons.email_outlined,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        // Password
+                        TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Ingrese su contraseña';
+                            }
+                            if (value.length < 6) {
+                              return 'La contraseña debe de tener al menos 6 caracteres';
+                            }
+                            return null;
+                          },
+                          obscureText: true,
+                          style: const TextStyle(color: Colors.white),
+                          decoration: CustomInputs.loginInputDecoration(
+                            hint: '******',
+                            label: 'Contraseña',
+                            icon: Icons.lock_outline_rounded,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        CustomOutlinedButton(
+                          onPressed: () {
+                            loginFormProvider.validateForm();
+                          },
+                          text: 'Ingresar',
+                        ),
+                        const SizedBox(height: 20),
+                        LinkText(
+                          text: 'Nueva cuenta',
+                          onPressed: () {
+                            Navigator.pushNamed(
+                                context, Flurorouter.registerRoute);
+                          },
+                        )
+                      ],
+                    )),
               ),
-              const SizedBox(height: 20),
-              // Password
-              TextFormField(
-                // validator: (value) => ,
-                obscureText: true,
-                style: const TextStyle(color: Colors.white),
-                decoration: CustomInputs.loginInputDecoration(
-                  hint: '******',
-                  label: 'Contraseña',
-                  icon: Icons.lock_outline_rounded,
-                ),
-              ),
-              const SizedBox(height: 20),
-              CustomOutlinedButton(
-                onPressed: () {},
-                text: 'Ingresar',
-              ),
-              const SizedBox(height: 20),
-              LinkText(
-                text: 'Nueva cuenta',
-                onPressed: () {
-                  Navigator.pushNamed(context, Flurorouter.registerRoute);
-                },
-              )
-            ],
-          )),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
